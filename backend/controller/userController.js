66554b6535dcc3d634b56a96f30d00c251d656ce
tyab07 <tyab.mujahid.com>
2026-03-14 13:49:08 +0500

    user controller added

---
+++ backend/controller/userController.js
@@ -0,0 +1,7 @@
+import {catchAsyncErrors} from "../middleware/catchAsyncErrors.js";
+import ErrorHandler from "../utils/errorHandler.js";
+import { User } from "../models/userSchema.js";
+import { v2 as cloudinary } from "cloudinary";
+
+
+ 
